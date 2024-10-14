4887a6a0a5854d44760791c0eb490b85cd17ed9c
Florian Salaun <florian.salaun@nokia.com>
2024-10-14 11:10:07 +0200

    Ypdated version to 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyMsTeamsAdaptiveCard',
-    version='0.1.6',
+    version='0.1.8',
     author='Florian Salaun',
     author_email='florian.salaun@gmail.com',
     packages=['pyadaptivecard', 'pyadaptivecard.test'],
